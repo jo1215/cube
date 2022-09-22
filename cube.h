7d30464ba9eb4741be900fd2eaaae6204175d539
Jason Ostdiek <jjostdiek@nevada.unr.edu>
2022-09-22 09:55:30 -0700

    lookup

--- cube.h
+++ cube.h
@@ -5,6 +5,7 @@ class Cube{
     private:
         int cube[6][9];
         int turnCount = 0;
+        std::string validMoves[67] = {"U","D","L","R","F","B","u","d","l","r","f","b","U'","D'","L'","R'","F'","B'","u'","d'","l'","r'","f'","b'","U2","D2","L2","R2","F2","B2","u2","d2","l2","r2","f2","b2","U2'","D2'","L2'","R2'","F2'","B2'","u2'","d2'","l2'","r2'","f2'","b2'","x","x'","x2","y","y'","y2","z","z'","z2","M","M'","M2","E","E'","E2","S","S'","S2","exit"};
     public:
         std::ofstream outputFile;
         Cube();
---
+++ makefile
@@ -0,0 +1,11 @@
+cube: main.o cube.o
+	g++ -o cube main.cpp cube.cpp
+
+main.o: main.cpp cube.h
+	g++ -c main.cpp
+
+cube.o: cube.cpp cube.h
+	g++ -c cube.cpp
+
+clean:
+	rm *.o cube
